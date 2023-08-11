7ab81df8d8098b7f1a7cd49a5d94d97bfdbe70b3
Vicky2114 <vr6994056@gmail.com>
2023-08-11 10:28:08 +0530

    vcd

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import LoginScreen from './Screens/LoginScreen';
+import Background from './Components/Background';
+
 
 function App() {
   return (
-    <div> 
-      <LoginScreen/>
+    <div style={{height:'100%'}}> 
+     <Background
+     entered = {true}/>
     </div>
   );
 }
