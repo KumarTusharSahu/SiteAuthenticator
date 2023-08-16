a8917dae7a50ee92e4527bd4e5feec9e15765d2b
KumarTusharSahu <tsahu2057@gmail.com>
2023-08-16 19:04:52 +0530

    tks

--- client/src/Screens/ForgetPasswordMailScreen.js
+++ client/src/Screens/ForgetPasswordMailScreen.js
@@ -1,25 +1,32 @@
 import axios from 'axios';
 import '../Assets/css/MailScreen.css';
+import forget from "../Assets/images/forget.webp";
+import { Link } from "react-router-dom";
 
+import 'animate.css';
 
 function ForgetPasswordMailScreen(){
     return (
-        <div className="row">
+      <>
+      <div className="forgetContainer">
+        <img src={forget} alt="vf" className="forgotImg animate__animated animate__slideInLeft" />
+        <div className="row animate__animated animate__slideInRight">
         <h1>Forgot Password</h1>
         <h6 className="information-text">Enter your registered email to reset your password.</h6>
         <div className="form-group">
         <form action='/users/link' method='post' >
-          <input type="email" name="email" id="user_email" />
+          <input type="email" name="email" id="user_email" placeholder="Enter your registered Email ID" />
           <p><label htmlFor="username">Email</label></p>
           <button onclick="showSpinner()">Reset Password</button>
           </form>
         </div>
         <div className="footer">
-          <h5>New here? <a href="#">Sign Up.</a></h5>
-          <h5>Already have an account? <a href="#">Sign In.</a></h5>
-          <p className="information-text"><span className="symbols" title="Lots of love from me to YOU!">♥ </span><a href="https://www.facebook.com/adedokunyinka.enoch" target="_blank" title="Connect with me on Facebook">Yinka Enoch Adedokun</a></p>
+          <h5>New here?  <Link to="/" className="forgetPass">  Sign Up</Link></h5>
+          <h5>Already have an account? <Link to="/" className="forgetPass"> Sign In</Link></h5>
         </div>
       </div>
+      </div>
+    </>
   )
 }
 
