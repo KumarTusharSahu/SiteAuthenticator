6de4616d3ac904d9fea5627117310764b601186c
KumarTusharSahu <tsahu2057@gmail.com>
2023-08-19 13:04:58 +0530

    tks

--- client/src/Components/NavBar.js
+++ client/src/Components/NavBar.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 function NavBar() {
   return (
-    <nav className="navbar navbar-expand-lg px-5 fixed-top">
+    <nav className="navbar navbar-expand-lg px-5 ">
       <a className="navbar-brand" href="#">SiteAuthenticator</a>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
