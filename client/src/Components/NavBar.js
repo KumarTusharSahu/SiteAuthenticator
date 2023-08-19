06607235f4e9a265b099dc50902f4b8c3a719326
KumarTusharSahu <tsahu2057@gmail.com>
2023-08-19 17:01:35 +0530

    tks

--- client/src/Components/NavBar.js
+++ client/src/Components/NavBar.js
@@ -4,8 +4,8 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 function NavBar() {
   return (
-    <nav className="navbar navbar-expand-lg px-5 ">
-      <a className="nav-item nav-link move" href="#" title="Profile"><i class="fa-solid fa-circle-user fa-2x"></i></a>
+    <nav className="navbar navbar-expand-lg px-5 fixed-top">
+      <a className="nav-item nav-link move" href="#" title="Profile"><i className="fa-solid fa-circle-user fa-2x"></i></a>
 
       <a className="navbar-brand mx-3" href="#">SiteAuthenticator</a>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -17,7 +17,7 @@ function NavBar() {
           <a className="nav-item nav-link move" href="#">Manual</a>
           <a className="nav-item nav-link move" href="#">About</a>
           <a className="nav-item nav-link move" href="#">Contact Us</a>
-          <a className="nav-item nav-link move" href="#" title="Logout"><i class="fa-solid fa-right-from-bracket"></i></a>
+          <a className="nav-item nav-link move" href="#" title="Logout"><i className="fa-solid fa-right-from-bracket"></i></a>
         </div>
       </div>
     </nav>
--- client/src/Screens/HomeScreen.js
+++ client/src/Screens/HomeScreen.js
@@ -5,14 +5,66 @@ import NavBar from '../Components/NavBar';
 
 import "../Assets/css/HomeScreen.css"
 
+import blocklist from "../Assets/images/grid/blockedlist.svg"
+import focusmode from "../Assets/images/grid/focusmode.svg"
+import passwordprotection from "../Assets/images/grid/passwordprotection.svg"
+import schedule from "../Assets/images/grid/schedule.svg"
+
 const HomeScreen = () => {
   return (
     <>
-    <div className="home">
-      <NavBar />
-      <div className='HomeScreenContent'
-      >
-        {/* <div className="homemain">
+    <NavBar />
+      <div className="home">
+        
+        <div className='HomeScreenContent'>
+          <h1 className="anim" style={{ color: "white" }}>Welcome to home screen</h1>
+          <Link to="/login" className="forgetPass links anim"> Sign In</Link>
+          <div className="links anim">
+            <div className="homecontent">
+              <button><span></span>Free Images</button>
+              <button><span></span>Image API</button>
+            </div>
+          </div>
+          
+        </div>
+        <div className='gridHeading'>
+        <h1>Your Productivity Tools</h1>
+        <p>Check the features SiteAuthenticator offers to help improve your productivity, stay focused, and avoid distractions</p>
+        </div>
+        <div className="gridContainer">
+            <div>
+              <img src={blocklist} alt="" />
+              <h1>Block List</h1>
+              <p>No limit to the sites and apps you can block</p>
+            </div>
+            <div>
+              <img src={focusmode} alt="" />
+              <h1>Focus Mode</h1>
+              <p>Prevent all distractions within a set amount of time</p>
+            </div>
+            <div>
+              <img src={schedule} alt="" />
+              <h1>Schedule</h1>
+              <p>Block all your distractions during the hours you choose</p>
+            </div>
+            <div>
+              <img src={passwordprotection} alt="" />
+              <h1>Password Protection</h1>
+              <p>When you’re tempted to change your settings, you’ll have to enter your password</p>
+            </div>
+          </div>
+        <Footer />
+
+      </div>
+      
+    </>
+  )
+}
+
+export default HomeScreen
+
+
+{/* <div className="homemain">
         <div className="homecontent">
           <h1 className="anim">Stock Photos</h1>
           <p className="anim">Beautiful, free images and photos that you can download and use for any project. <br /> than any royalty free or stock photos.</p>
@@ -22,20 +74,3 @@ const HomeScreen = () => {
           </div>
         </div>
       </div> */}
-        <h1 className="anim" style={{ color: "white" }}>Welcome to home screen</h1>
-        <Link to="/login" className="forgetPass links anim"> Sign In</Link>
-        <div className="links anim">
-        <div className="homecontent">
-          <button><span></span>Free Images</button>
-          <button><span></span>Image API</button>
-          </div>
-        </div>
-      </div>
-      <Footer />
-
-</div>
-    </>
-  )
-}
-
-export default HomeScreen
